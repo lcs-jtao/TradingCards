b91eeed418d2126770cf37cb9e387a7d1a0d01f0
Joyce Tao <jtao@lcs.on.ca>
2021-11-09 11:18:52 -0500

    Created needed files

---
+++ TradingCards/Views/Model/TradingCards.swift
@@ -0,0 +1,20 @@
+//
+//  TradingCards.swift
+//  TradingCards
+//
+//  Created by Joyce Tao on 2021-11-09.
+//
+
+import SwiftUI
+
+struct TradingCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TradingCards_Previews: PreviewProvider {
+    static var previews: some View {
+        TradingCards()
+    }
+}
