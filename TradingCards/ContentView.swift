b8000968a7c830b35487488bcec029ba7ac34d3f
Joyce Tao <jtao@lcs.on.ca>
2021-11-09 11:07:38 -0500

    Initial Commit

---
+++ TradingCards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by Joyce Tao on 2021-11-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Joyce Tao on 2021-11-09.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
